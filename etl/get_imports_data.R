98b93140f99e8985da0a7521be268a55dffb94bc
murray-cecile <murra394@60HQPLSU4440645.boc.ad.census.gov>
2022-11-14 17:30:54 -0500

    imports code working

--- etl/get_imports_data.R
+++ etl/get_imports_data.R
@@ -18,6 +18,24 @@ invisible(suppressMessages(lapply(libs, library, character.only=TRUE)))
 census_key <- Sys.getenv("CENSUS_API_KEY")
 
 
+# debug
+porths_endpoint <- "https://api.census.gov/data/timeseries/intltrade/imports/porths"
+
+
+req <- httr::GET(porths_endpoint,
+          query = list(
+            key = census_key,
+            get = "PORT,I_COMMODITY,I_COMMODITY_LDESC,GEN_VAL_MO",
+            time = "2022-09",
+            COMM_LVL = "HS6",
+            I_COMMODITY = "200989"
+          )
+)
+req$url
+raw <- jsonlite::fromJSON(httr::content(req, as = "text"))
+jsonlite::validate(httr::content(req, as = "text"))
+
+
 # US totals of imports by six-digit HS code in September of 2022
 natl <- getCensus(
   name = "timeseries/intltrade/imports/hs",
@@ -31,18 +49,15 @@ natl <- getCensus(
   I_COMMODITY = "*"
 )
 
-
-# imports from ports?
-# seems to be a parsing issue with censusapi. see if I can resolve?
+# imports from ports - SLOW!
 raw_imports <- map_dfr(
-  natl$I_COMMODITY,  
+  natl$I_COMMODITY,
   ~ getCensus(
     name = "timeseries/intltrade/imports/porths",
     key = census_key,
     vars = c("PORT",
              "I_COMMODITY",
-             "I_COMMODITY_LDESC",
-             "I_COMMODITY_SDESC",
+             "I_COMMODITY_SDESC", 
              "GEN_VAL_MO",
              "GEN_VAL_YR"
     ),
@@ -50,3 +65,56 @@ raw_imports <- map_dfr(
     I_COMMODITY = .
   )
 )
+# NOTE: issue parsing LDESC field from json
+
+raw_imports %>% glimpse()
+raw_imports %>% saveRDS("data/imports_HS6_ports_2022-09.Rds")
+
+
+ports_xwalk <- getCensus(
+  name = "timeseries/intltrade/imports/porths",
+  key = census_key,
+  vars = c("PORT", "PORT_NAME"),
+  time = "2022-09"
+) %>% 
+  mutate(st = str_extract(PORT_NAME, "(?<=, )\\w{2}$")) %>% 
+  select(-time)
+
+fl_imports <- raw_imports %>% 
+  inner_join(
+    ports_xwalk %>% filter(st == "FL"),
+    by = "PORT"
+  )
+
+# what's the top commodity at FL ports?
+fl_imports %>% 
+  mutate(GEN_VAL_MO = as.numeric(GEN_VAL_MO)) %>% 
+  group_by(
+    I_COMMODITY,
+    I_COMMODITY_SDESC
+  ) %>% 
+  summarize(
+    gen_val_mo = sum(GEN_VAL_MO)
+  ) %>% 
+  arrange(-gen_val_mo) %>% 
+  head(10)
+
+fl_summary <- fl_imports %>% 
+  mutate(GEN_VAL_MO = as.numeric(GEN_VAL_MO)) %>% 
+  group_by(
+    I_COMMODITY,
+    I_COMMODITY_SDESC
+  ) %>% 
+  summarize(
+    fl_gen_val_mo = sum(GEN_VAL_MO)
+  ) %>% 
+  ungroup()
+
+# shares at FL ports
+fl_shares <- natl %>% 
+  left_join(
+    fl_summary
+  ) %>% 
+  mutate(
+    fl_share = fl_gen_val_mo / as.numeric(GEN_VAL_MO)
+  )
