43f1fde36281e641e674a869835ab7698866af03
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2022-11-23 17:55:10 -0500

    add port-county crosswalk + imports data for tableau

---
+++ analysis/compute_import_shares.R
@@ -0,0 +1,110 @@
+#===============================================================================#
+# COMPUTE IMPORT SHARES
+# Depends: etl/get_imports_data.R, etl/put_ports_in_county_geo.R
+# 2022-11-23
+# Cecile Murray
+#===============================================================================#
+
+libs <- c(
+  "tidyverse",
+  "janitor",
+  "sf",
+  "tidycensus",
+  "tmap"
+)
+lapply(libs, library, character.only = TRUE)
+
+
+# input file paths
+NATL_HS6_FILE <- "data/imports_HS6_natl_2022-09.Rds"
+PORTS_HS6_FILE <- "data/imports_HS6_ports_2022-09.Rds"
+PORT_XWALK_FILE <- "data/major_port_county_xwalk.Rds"
+
+# read in data
+natl_imports <- readRDS(NATL_HS6_FILE)
+raw_imports <- readRDS(PORTS_HS6_FILE)
+ports_xwalk <- readRDS(PORT_XWALK_FILE)
+
+
+# ANALYSIS   =============================#
+
+fl_imports <- raw_imports %>% 
+  inner_join(
+    ports_xwalk %>% filter(STATEFP == "12"),
+    by = "PORT"
+  )
+
+# what's the top commodity at FL ports?
+fl_summary <- fl_imports %>% 
+  mutate(
+    GEN_VAL_MO = as.numeric(GEN_VAL_MO),
+    GEN_VAL_YR = as.numeric(GEN_VAL_YR),
+  )  %>% 
+  group_by(
+    I_COMMODITY,
+    I_COMMODITY_SDESC
+  ) %>% 
+  summarize(
+    fl_gen_val_mo = sum(GEN_VAL_MO),
+    fl_gen_val_yr = sum(GEN_VAL_YR),
+  ) %>% 
+  ungroup()
+
+# shares at FL ports
+fl_shares <- fl_imports %>% 
+  left_join(
+    natl_imports,
+    suffix = c("_fl", ""),
+    by = c(
+      "I_COMMODITY",
+      "I_COMMODITY_SDESC",
+      "I_COMMODITY_1"
+    )
+  ) %>% 
+mutate(
+    fl_share_mo = as.numeric(GEN_VAL_MO_fl) / as.numeric(GEN_VAL_MO),
+    fl_share_yr = as.numeric(GEN_VAL_YR_fl) / as.numeric(GEN_VAL_YR),
+  )
+
+fl_shares %>%
+  select(I_COMMODITY_SDESC, contains("GEN_VAL_MO"), contains("fl_share")) %>% 
+  View()
+
+natl_imports %>% 
+  mutate_at(
+    vars(contains("GEN_VAL")),
+    ~ as.numeric(.)
+  ) %>% 
+  View()
+
+# PRODUCE STATISTICS FOR TABLEAU  =============================#
+
+# compute the share of products coming in through each port from national total 
+# in Sept and year to date
+
+port_shares <- raw_imports %>% 
+  left_join(
+    natl_imports,
+    suffix = c("_port", ""),
+    by = c(
+      "I_COMMODITY",
+      "I_COMMODITY_SDESC",
+      "I_COMMODITY_1",
+      "time"
+    )
+  ) %>% 
+  mutate(
+    port_share_mo = as.numeric(GEN_VAL_MO_port) / as.numeric(GEN_VAL_MO) * 100,
+    port_share_yr = as.numeric(GEN_VAL_YR_port) / as.numeric(GEN_VAL_YR) * 100,
+  )
+
+port_shares %>% 
+  filter(PORT != "-") %>%
+  select(
+    PORT,
+    I_COMMODITY,
+    I_COMMODITY_SDESC,
+    contains("GEN_VAL"),
+    contains("_share_")
+  ) %>%
+  View()
---
+++ etl/put_ports_in_county_geo.R
@@ -0,0 +1,57 @@
+#===============================================================================#
+# PERFORM SPATIAL JOIN OF PORTS TO COUNTIES
+# 2022-11-23
+# Cecile Murray
+#===============================================================================#
+
+libs <- c(
+  "here",
+  "tidyverse",
+  "janitor",
+  "tigris",
+  "sf"
+)
+lapply(libs, library, character.only = TRUE)
+
+PORT_XWALK_FILE <- "data/major_port_county_xwalk.Rds"
+
+# county shp from TIGER-line
+cty <- counties()
+
+# read shapefile from BTS website
+ports_shp <- st_read("data/Principal_Port.shp")
+
+# CHECK CRS =============================#
+
+if (st_crs(cty) == st_crs(ports_shp)) {
+  print("Coordinate systems match, nothing to do")
+} else {
+  
+  print("Coordinate systems don't match; reprojecting")
+  # reproject the ports one into the census one
+  # this magic number is the CRS ID for WGS 1983, can be found in the output
+  # from st_crs(cty)
+  ports_shp <- ports_shp %>% st_transform(4269)
+  
+}
+  
+# check coordinate reference system again
+assertthat::assert_that(st_crs(cty) == st_crs(ports_shp))
+
+# PERFORM JOIN  =============================#
+
+port_in_cty <- st_join(ports_shp, cty)
+
+# get a simple tibble without spatial features, all string types
+port_cty_df <- port_in_cty %>% 
+  select(
+    PORT, 
+    RANK,
+    GEOID,
+    STATEFP,
+    NAMELSAD
+  ) %>% 
+  st_drop_geometry() %>% 
+  mutate_all(~ as.character(.))
+
+port_cty_df %>% saveRDS(PORT_XWALK_FILE)
